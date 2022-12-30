184a02ee195bab351f54cff4d276fca9ec57aa45
dmikhailov89 <dmikhailov89@gmail.com>
2022-12-30 17:03:59 +0300

    Updated algorithm for number guessing

--- guess-number-task/game_v2.py
+++ guess-number-task/game_v2.py
@@ -5,22 +5,31 @@
 import numpy as np
 
 
-def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
+def random_predict(number: int = np.random.randint(1, 101)) -> int:
+    """Компьютер или пользователь загадывает число
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): Загаданное число. По умаолчанию рандом от 0 до 100
 
     Returns:
         int: Число попыток
     """
     count = 0
+    
+    # заготовим переменные для ограничения "отгадывателя"
+    mx = 101 
+    mn = 1
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
+        predict_number = np.random.randint(mn, mx)  # предполагаемое число
+        if number < predict_number: # если предполагаемое число больше загаданного, то оно становится новым ограничением сверху
+            mx = predict_number 
+        elif number > predict_number: # если предполагаемое число меньше загаданного, то оно становится новым ограничением снизу
+            mn = predict_number
+        else:          
             break  # выход из цикла если угадали
+        print(number, mn, mx, count)
     return count
 
 
@@ -43,8 +52,3 @@ def score_game(random_predict) -> int:
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
-
-
-if __name__ == "__main__":
-    # RUN
-    score_game(random_predict)
